9fc9210c6a85f30cb482352770d8ea9a875eeb47
Karan <packirisamykaran@gmail.com>
2023-04-11 14:51:10 +0800

    retrieve

--- fundRaiser/index.js
+++ fundRaiser/index.js
@@ -1,36 +1,72 @@
 const dotenv = require("dotenv");
+const fs = require("fs");
+const csv = require("csv-parser");
 dotenv.config();
 
-var Airtable = require("airtable");
-Airtable.configure({
-    endpointUrl: "https://api.airtable.com",
-    apiKey: process.env.AIRTABLE_API_KEY,
-});
-var base = Airtable.base("appxVDi54fHsOkrp3");
+let result = [];
 
-base("Ramp's VC & Angel Email Database")
-    .select({
-        // Selecting the first 3 records in Grid view:
-        maxRecords: 300,
-        view: "Grid view",
+let final = [];
+
+fs.createReadStream("./source.csv")
+    .pipe(csv())
+    .on("data", (data) => {
+        result.push(data);
     })
-    .eachPage(
-        function page(records, fetchNextPage) {
-            // This function (`page`) will get called for each page of records.
+    .on("end", () => {
+        for (var index in result) {
+            let vc = result[index];
 
-            records.forEach(function (record) {
-                console.log("Retrieved", record.get("Investor Name"));
-            });
+            let row = {};
 
-            // To fetch the next page of records, call `fetchNextPage`.
-            // If there are more records, `page` will get called again.
-            // If there are no more records, `done` will get called.
-            fetchNextPage();
-        },
-        function done(err) {
-            if (err) {
-                console.error(err);
-                return;
-            }
+            // Investor Name
+            row.name = vc["Investor Name"];
+
+            // description
+            row.description = vc["Fund Description"];
+
+            // AUM
+            row.aum = 0;
+
+            // investment mandate
+            let mandate = {};
+
+            mandate.fundingStages = vc["Fund Stage"];
+            mandate.targetRegion = vc[""];
+
+            break;
         }
-    );
+    });
+
+// var Airtable = require("airtable");
+// Airtable.configure({
+//     endpointUrl: "https://api.airtable.com",
+//     apiKey: process.env.AIRTABLE_API_KEY,
+// });
+// var base = Airtable.base("appxVDi54fHsOkrp3");
+// s;
+// base("Ramp's VC & Angel Email Database")
+//     .select({
+//         // Selecting the first 3 records in Grid view:
+//         maxRecords: 300,
+//         view: "Grid view",
+//     })
+//     .eachPage(
+//         function page(records, fetchNextPage) {
+//             // This function (`page`) will get called for each page of records.
+
+//             records.forEach(function (record) {
+//                 console.log("Retrieved", record.get("Investor Name"));
+//             });
+
+//             // To fetch the next page of records, call `fetchNextPage`.
+//             // If there are more records, `page` will get called again.
+//             // If there are no more records, `done` will get called.
+//             fetchNextPage();
+//         },
+//         function done(err) {
+//             if (err) {
+//                 console.error(err);
+//                 return;
+//             }
+//         }
+//     );
